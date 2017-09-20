e847984baf4cbc4ad620aca4f4bf4377dc55a3ea
Antonia Patterson <apatter7@gmail.com>
2017-09-20 17:54:26 -0400

    splash page

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$(".slackBtn").click(function(){
+  $(".slackBtn").text("SLACKTASTIC!!!");
+        });
+});
+
+$(document).ready(function(){
+$(".slackBtnOne").click(function(){
+  $(".slackBtnOne").text("SLACKTASTIC!!!");
+        });
+});
